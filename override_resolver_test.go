b3c13e026d0a1d01fc03adb87c82de2a796ff315
Matt Edwards <mathew.edwards@comparethemarket.com.au>
2019-12-27 10:36:31 +1000

    TEST: Updates OverrideEndpointresolver tests to use table-based tests

--- override_resolver_test.go
+++ override_resolver_test.go
@@ -8,33 +8,49 @@ import (
 	"github.com/edwardsmatt/dynamocity"
 )
 
-func Test_OverrideResolver(t *testing.T) {
-
-	overrides := make(map[string]string)
-	overrides[dynamodb.EndpointsID] = "http://localhost:8000"
-
-	r := dynamocity.MakeEndpointResolver(overrides)
-	endpoint, err := r.ResolveEndpoint(dynamodb.EndpointsID, endpoints.ApSoutheast2RegionID)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
+func Test_OverrideEndpointResolver(t *testing.T) {
+	cases := []struct {
+		description         string
+		region              string
+		endpointID          string
+		overrides           map[string]string
+		expectedEndpointURL string
+	}{
+		{
+			description: "Given an overridden DynamoDB endpoint, when using ap-southeast-2 region, then return the expected endpoint",
+			region:      endpoints.ApSoutheast2RegionID,
+			endpointID:  dynamodb.EndpointsID,
+			overrides: map[string]string{
+				dynamodb.EndpointsID: "http://localhost:8000",
+			},
+			expectedEndpointURL: "http://localhost:8000",
+		},
+		{
+			description:         "Given no overridden DynamoDB endpoint, when using ap-southeast-2 region, then return the endpoint for the the region",
+			region:              endpoints.ApSoutheast2RegionID,
+			endpointID:          dynamodb.EndpointsID,
+			overrides:           make(map[string]string),
+			expectedEndpointURL: "https://dynamodb.ap-southeast-2.amazonaws.com",
+		},
+		{
+			description:         "Given no overridden DynamoDB endpoint, when using us-east-1 region, then return the endpoint for the the region",
+			region:              endpoints.UsEast1RegionID,
+			endpointID:          dynamodb.EndpointsID,
+			overrides:           make(map[string]string),
+			expectedEndpointURL: "https://dynamodb.us-east-1.amazonaws.com",
+		},
 	}
 
-	if endpoint.URL != overrides[dynamodb.EndpointsID] {
-		t.Errorf("Unexpected endpoint. Expected '%s', Got '%s'", overrides[dynamodb.EndpointsID], endpoint.URL)
-	}
-}
+	for _, tc := range cases {
+		r := dynamocity.MakeEndpointResolver(tc.overrides)
+		endpoint, err := r.ResolveEndpoint(tc.endpointID, tc.region)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-func Test_OverrideResolverWithNoOverride(t *testing.T) {
-
-	r := dynamocity.MakeEndpointResolver(make(map[string]string))
-	endpoint, err := r.ResolveEndpoint(dynamodb.EndpointsID, endpoints.ApSoutheast2RegionID)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-
-	if endpoint.URL != "https://dynamodb.ap-southeast-2.amazonaws.com" {
-		t.Errorf("Unexpected endpoint. Expected '%s', Got '%s'", "https://dynamodb.ap-southeast-2.amazonaws.com", endpoint.URL)
+		if endpoint.URL != tc.expectedEndpointURL {
+			t.Errorf("Unexpected endpoint. Expected '%s', Got '%s'", tc.expectedEndpointURL, endpoint.URL)
+		}
 	}
 }
