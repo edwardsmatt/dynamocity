0e23856c209b41ac4d0c7d6cf1e033c47be9c069
Matt Edwards <edwardsmatt@git>
2020-11-05 14:46:31 +1000

    bug(Date): Adds flexible unmarshaller for Date

--- date.go
+++ date.go
@@ -10,9 +10,9 @@ import (
 
 // Date represents a sortable Date with fixed date precision.
 //
-// Date implements dynamodbattribute.Marshaler, dynamodbattribute.Unmarshaller specifically for "YYYY-MM-DD"
-// format which does not permit any timestamp; however, once this format is marshalled it may be sorted correctly in a
-// string value
+// Date implements dynamodbattribute.Marshaler specifically for "YYYY-MM-DD"
+// format which does not permit any timestamp; however, it can unmarshall from any
+// Timestamp with nanosecond precision.
 type Date time.Time
 
 // MarshalDynamoDBAttributeValue implements the dynamodb.Marshaler interface to marshal
@@ -28,12 +28,24 @@ func (t Date) MarshalDynamoDBAttributeValue(av *dynamodb.AttributeValue) error {
 // with nanosecond precision
 func (t *Date) UnmarshalDynamoDBAttributeValue(av *dynamodb.AttributeValue) error {
 	timeString := *av.S
-	date, err := time.Parse(FlexibleNanoFmt, timeString)
+	date, err := parse(timeString)
 	if err != nil {
 		return err
 	}
 	*t = Date(date)
 	return nil
+}
+
+// parse is a helper function to assist with parsing a string to a time.Time.
+//
+// This function will attempt to parse using dynamocity.FlexibleNanoFmt, and if that fails
+// dynamocity.StrictDateFmt
+func parse(str string) (time.Time, error) {
+	parsedTime, err := time.Parse(FlexibleNanoFmt, str)
+	if err == nil {
+		return parsedTime, nil
+	}
+	return time.Parse(StrictDateFmt, str)
 }
 
 // Time is a handler func to return an instance of dynamocity.Date as time.Time
@@ -47,15 +59,15 @@ func (t Date) String() string {
 	return t.Time().Format(StrictDateFmt)
 }
 
-// UnmarshalJSON implements the json.Unmarshaler interface to marshal RFC3339 timestamps with second precision
+// UnmarshalJSON implements the json.Unmarshaler interface to unmarshal a date or RFC3339 timestamp
 func (t *Date) UnmarshalJSON(b []byte) error {
 	str, err := strconv.Unquote(string(b))
 	if err != nil {
 		return err
 	}
-	parsedTime, err := time.Parse(FlexibleNanoFmt, str)
+	parsedTime, err := parse(str)
 	if err != nil {
-		return fmt.Errorf("Timestamp '%s' cannot be unmarshalled as a valid RFC3339 timestamp", str)
+		return fmt.Errorf("Timestamp '%s' cannot be unmarshalled", str)
 	}
 	*t = Date(parsedTime)
 	return nil
@@ -64,4 +76,10 @@ func (t *Date) UnmarshalJSON(b []byte) error {
 // MarshalJSON implements the json.Marshaler interface to marshal RFC3339 timestamps with second precision
 func (t Date) MarshalJSON() ([]byte, error) {
 	return []byte(strconv.Quote(t.String())), nil
+}
+
+// ParseDate will attempt to parse any RFC3339 Timestamp or date with format YYYY-MM-DD to a dynamocity.Date
+func ParseDate(str string) (Date, error) {
+	time, err := parse(str)
+	return Date(time), err
 }
